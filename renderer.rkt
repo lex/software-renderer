4e16cb127c08b097f17d0a3ee85a3c37fae94ed8
lex <lex@oispa.beer>
2017-03-26 21:24:17 +0300

    add some lines

--- renderer.rkt
+++ renderer.rkt
@@ -1,15 +1,57 @@
 #lang racket/gui
 
+(define WIDTH 640)
+(define HEIGHT 480)
+
+(define-syntax-rule (swap x y)
+  (let ([tmp x])
+    (set! x y)
+    (set! y tmp)))
+
 (define frame (new frame%
                    [label "hello world"]
-                   [width 640]
-                   [height 480]))
+                   [width WIDTH]
+                   [height HEIGHT]))
+
+(define (create-pen r g b w)
+  (new pen% [color (make-color r g b)] [width w]))
+
+(define (pen-from-color color)
+  (cond
+    [(eq? color "red") (create-pen 255 0 0 1)]
+    [(eq? color "black") (create-pen 0 0 0 1)]
+    [else (create-pen 0 255 0 1)]))
+
+(define (draw-line dc x0 y0 x1 y1)
+  (define steep #false)
+
+  (when (< (abs (- x0 x1)) (abs (- y0 y1)))
+    (swap x0 y0)
+    (swap x1 y1)
+    (set! steep #true))
+
+  (when (> x0 x1)
+    (swap x0 x1)
+    (swap y0 y1))
+
+  (send dc set-pen (pen-from-color "red"))
+
+  (for ([x (in-range x0 (+ x1 1))])
+    (define t (/ (- x x0) (- x1 x0)))
+    (define y (+ (* y0 (- 1.0 t)) (* y1 t)))
+
+    (if 'steep
+        (send dc draw-point y x)
+        (send dc draw-point x y))))
 
 (new canvas% [parent frame]
              [paint-callback
               (lambda (canvas dc)
-                (send dc set-scale 3 3)
-                (send dc set-text-foreground "red")
-                (send dc draw-text "hello world" 0 0))])
+                (send dc set-pen (pen-from-color "black"))
+                (send dc set-brush "black" 'solid)
+                (send dc draw-rectangle 0 0 WIDTH HEIGHT)
+                (draw-line dc 13 20 80 40)
+                (draw-line dc 40 50 80 90)
+                (draw-line dc 100 10 50 60))])
 
 (send frame show #t)
